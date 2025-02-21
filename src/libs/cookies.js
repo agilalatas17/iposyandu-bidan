fbcc69f775c103d333b7d221287b172131232c09
agilalatas <agilalatas17@gmail.com>
2025-02-22 03:25:33 +0700

    membuat konfigurasi cookies

---
+++ src/libs/cookies.js
@@ -0,0 +1,25 @@
+'use server';
+import { cookies } from 'next/headers';
+
+export const setCookie = async (name, value, option = {}) => {
+  const cookieStore = await cookies();
+
+  cookieStore.set(name, value, {
+    ...option,
+    httpOnly: option.httpOnly || true,
+  });
+};
+
+export const getCookie = async (name) => {
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get(name);
+
+  return cookie;
+};
+
+export const hasCookie = async (name) => {
+  const cookieStore = await cookies();
+  const cookie = cookieStore.has(name);
+
+  return cookie;
+};
